88b9536693a05a925f71b112d0f7ee1e7a1c3ff9
Songwut Maneefun <songwut.m@gmail.com>
2020-02-28 17:44:51 +0700

    create

---
+++ android/app/src/main/kotlin/com/example/co_story_flutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.co_story_flutter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
